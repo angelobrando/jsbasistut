14e368324d4692b38d075e5cb34cba6da0bb7d0e
angelobrando <yasyas112>
2019-02-20 02:47:15 +0100

    3.1

--- jsscript.js
+++ jsscript.js
@@ -1,15 +1,14 @@
-var box1 = document.getElementById("bl1");
-var box2 = document.getElementById("bl2");
-var box3 = document.getElementById("bl3");
-var box4 = document.getElementById("bl4");
-//box1
-box1.style.backgroundColor = "Aqua";
-//box2
-box2.style.marginTop = "20px";
-box2.style.backgroundColor = "green";
-//box3
-box3.style.marginTop = "50px";
-box3.style.backgroundColor = "yellow";
-//box4
-box4.style.backgroundColor = "red";
-console.log(box1);
+var images = document.getElementsByTagName("img");
+var pandomRumbers = [];
+
+while (pandomRumbers.length < 9){
+    var randomnumber = Math.floor(Math.random()*9 +1);
+    if (pandomRumbers.lastIndexOf(randomnumber) == -1 ) {
+        pandomRumbers.push(randomnumber)
+    }
+}
+console.log(pandomRumbers)
+for (let i = 0; i < images.length; i++) {
+    console.log(pandomRumbers[i]);
+    images[i].src = "img/aap" + pandomRumbers[i] + ".jpg"
+}
